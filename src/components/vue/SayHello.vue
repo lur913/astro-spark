bd839a1f8f31092a04e3198d58e8871911e03e7a
Ryan Lu <ryanlu365@gmail.com>
2025-05-12 20:16:06 +0800

    integrate vue

---
+++ src/components/vue/SayHello.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-3xl font-bold underline text-green-600 pt-3">{{ msg }}</div>
+  <button @click="sayHello" class="bg-blue-500 text-white p-2 rounded-md cursor-pointer">say hello</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const msg = ref('')
+const sayHello = () => {
+  if (msg.value === '') {
+    msg.value = 'Hello, Vue!'
+  } else {
+    msg.value = ''
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
